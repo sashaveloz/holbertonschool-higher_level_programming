c026490a3ebe2df38fa29b878c603b231c1f144e
sashaveloz <2065@holbertonschool.com>
2020-09-05 05:14:04 +0000

    Problem fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,6 @@ def uppercase(str):
     for index in str:
         letter = ord(index)
         if (letter > 96 and letter < 123):
-            print('{:c}'.format(letter), end='')
+            letter = letter - 32
+        print('{:c}'.format(letter), end='')
+    print()
