646a283cc1f97d5543c292deb24bb58da25ce3a8
sashaveloz <2065@holbertonschool.com>
2020-09-21 22:51:46 +0000

    Initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        idx = 0
+        while idx < x:
+            print(my_list[idx], end='')
+            idx += 1
+        print()
+    except:
+        print()
+    return idx
